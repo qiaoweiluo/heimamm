7341c1016764abc67db6841bea841f12f3e39d43
CCQQ1 <630688213@qq.com>
2019-11-19 18:33:50 +0800

    整合路由

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 导入抽取的路由
+
+import router from './router/router.js'
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  // 挂载到Vue实例
+  router
 }).$mount('#app')
---
+++ src/router/router.js
@@ -0,0 +1,21 @@
+// 导入vue-router
+import VueRouter from 'vue-router'
+// 导入vue
+import Vue from 'vue';
+
+// use
+Vue.use(VueRouter)
+// 规则
+const routes = [
+    // path: "",
+    // component: 
+]
+// 创建路由实例
+const router = new VueRouter({
+    routes
+})
+// 挂载到路由实例上
+// 暴露出去
+
+// 类似于 module.exports = routera
+export default router;
