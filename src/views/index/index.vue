3560ae2ec758811d94e52c24e4b81aa2e4fcc18c
CCQQ1 <630688213@qq.com>
2019-11-22 11:43:15 +0800

    基础布局

---
+++ src/views/index/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-container class="index-container">
+    <el-header class="header">Header</el-header>
+    <el-container>
+      <el-aside width="200px" class="aside">Aside</el-aside>
+      <el-main class="main">Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: "index"
+};
+</script>
+
+<style lang="less">
+.index-container{
+    height: 100%;
+    .header{
+        background-color: orange;
+    }
+    .aside{
+        background: skyblue;
+    }
+    .main{
+        background-color: hotpink;
+    }
+}
+</style>
