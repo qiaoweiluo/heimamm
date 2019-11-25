137a1eaa195325247a30ebfab29b4de9ca062196
CCQQ1 <630688213@qq.com>
2019-11-25 11:13:07 +0800

    完成 学科列表 分页数据获取

--- src/api/api.js
+++ src/api/api.js
@@ -2,11 +2,16 @@
 import axios from 'axios'
 
 // 导入 token获取方法
-import { getToken,removeToken } from "../utils/token.js";
+import {
+  getToken,
+  removeToken
+} from "../utils/token.js";
 
 // 如果你希望引入部分组件 可以使用按需导入
 // 导入 element-ui的弹框  对象解构
-import { Message } from "element-ui";
+import {
+  Message
+} from "element-ui";
 
 // 在main.js里面use过一次就不用重复use了
 // // 导入Vue
@@ -27,74 +32,119 @@ axios.defaults.withCredentials = true;
 // config 是当前这次请求的 各种设置
 // 可以统一设置header
 axios.interceptors.request.use(
-    function(config) {
-      // 为config 统一设置token
-      config.headers.token = getToken();
-      return config;
-    },
-    function(error) {
-      return Promise.reject(error);
+  function (config) {
+    // 为config 统一设置token
+    config.headers.token = getToken();
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+// 响应拦截器
+// axios 直接使用的.then之前
+// response 服务器响应的内容
+axios.interceptors.response.use(
+  function (response) {
+    // 判断token
+    if (response.data.code === 0) {
+      // 提示
+      Message.error('小老弟，伪造token，牛逼啊！')
+      // 删除token
+      removeToken();
+      // 去登录页
+      router.push("/login");
+      return;
     }
-  );
-  
-  // 响应拦截器
-  // axios 直接使用的.then之前
-  // response 服务器响应的内容
-  axios.interceptors.response.use(
-    function(response) {
-      // 判断token
-      if (response.data.code === 0) {
-          // 提示
-          Message.error('小老弟，伪造token，牛逼啊！')
-        // 删除token
-        removeToken();
-        // 去登录页
-        router.push("/login");
-        return;
-      }
-      return response;
-    },
-    function(error) {
-      return Promise.reject(error);
-    }
-  );
+    return response;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
 
 // 暴露出去 有名字的暴露 名字一般和接口地址一样
 export function login(data) {
-    return axios({
-        url:"/login",
-        method:"post",
-        data
-    })
+  return axios({
+    url: "/login",
+    method: "post",
+    data
+  })
 }
 
 // 暴露出去的 注册方法
-export function register(data){
-    return axios({
-        url:"/register",
-        method:"post",
-        data
-    })
+export function register(data) {
+  return axios({
+    url: "/register",
+    method: "post",
+    data
+  })
 }
 
 // 暴露出去的 获取短信验证码的方法
-export function sendsms(data){
-    return axios({
-        url:"/sendsms",
-        method:"post",
-        data
-    })
+export function sendsms(data) {
+  return axios({
+    url: "/sendsms",
+    method: "post",
+    data
+  })
 }
 
 // 获取用户信息
 export function userInfo() {
-    return axios({
-      url: "/user/info",
-      // 文档有问题，其实是get
-      method: "get",
-      // 通过拦截器设置了
+  return axios({
+    url: "/user/info",
+    // 文档有问题，其实是get
+    method: "get",
+    // 通过拦截器设置了
     //   headers:{
     //     token: getToken()
     //   }
+  });
+}
+
+// 暴露学科相关的api
+export const subjectApi = {
+  // 新增学科
+  add(data) {
+    return axios({
+      url: "/subject/add",
+      method: "post",
+      data
     });
+  },
+  // 学科列表
+  list(params) {
+    return axios({
+      url: "/subject/list",
+      method: "get",
+      params
+    });
+  },
+  // 状态修改
+  status(data) {
+    return axios({
+      url: "/subject/status",
+      method: "post",
+      data
+    })
+  },
+  // 学科编辑
+  edit(data) {
+    return axios({
+      url: "/subject/edit",
+      method: "post",
+      data
+    })
+  },
+  // 学科删除
+  remove(data) {
+    return axios({
+      url: "/subject/remove",
+      method: "post",
+      data
+    })
   }
+
+};
--- src/views/index/subject/subject.vue
+++ src/views/index/subject/subject.vue
@@ -2,55 +2,66 @@
   <div class="subject-container">
     <!-- 头部 -->
     <el-card>
+      <!-- 行内 表单 -->
       <el-form :inline="true" :model="formInline" class="demo-form-inline">
         <el-form-item label="学科编号">
-          <el-input v-model="formInline.user"></el-input>
+          <el-input v-model="formInline.rid"></el-input>
         </el-form-item>
         <el-form-item label="学科名称" class="more-width">
-          <el-input v-model="formInline.user"></el-input>
+          <el-input v-model="formInline.name"></el-input>
         </el-form-item>
         <el-form-item label="创建者">
-          <el-input v-model="formInline.user"></el-input>
+          <el-input v-model="formInline.creater"></el-input>
         </el-form-item>
         <el-form-item label="状态" class="more-width">
-          <el-select v-model="formInline.region" placeholder="请选择状态">
-            <el-option label="启用" value="1"></el-option>
-            <el-option label="禁用" value="0"></el-option>
+          <!-- 表单元素数据的绑定 是v-model -->
+          <el-select v-model="formInline.status" placeholder="请选择状态">
+            <el-option label="启用" :value="1"></el-option>
+            <el-option label="禁用" :value="0"></el-option>
           </el-select>
         </el-form-item>
-
         <el-form-item>
-          <el-button type="primary" @click="onSearch">查询</el-button>
+          <el-button type="primary" @click="search">搜索</el-button>
           <el-button>清除</el-button>
-          <el-button type="primary" icon="el-icon-plus">新增企业</el-button>
+          <el-button type="primary" icon="el-icon-plus">新增学科</el-button>
         </el-form-item>
       </el-form>
     </el-card>
     <!-- 内容 -->
     <el-card class="main-card">
+      <!-- 表格 -->
       <el-table :data="tableData" style="width: 100%" stripe border>
-        <el-table-column type="index" label="序号"></el-table-column>
-        <el-table-column prop="date" label="学科标号"></el-table-column>
-        <el-table-column prop="name" label="学科名称"></el-table-column>
-        <el-table-column prop="address" label="简称"></el-table-column>
-        <el-table-column prop="skill" label="创建者"></el-table-column>
-        <el-table-column prop="skill" label="创建日期"></el-table-column>
-        <el-table-column label="状态"></el-table-column>
+        <el-table-column type="index" label="序号"> </el-table-column>
+        <el-table-column prop="rid" label="学科编号"> </el-table-column>
+        <el-table-column prop="name" label="学科名称"> </el-table-column>
+        <el-table-column prop="short_name" label="简称"> </el-table-column>
+        <el-table-column prop="creater" label="创建者"> </el-table-column>
+        <el-table-column prop="create_time" label="创建日期"> </el-table-column>
+        <el-table-column label="状态">
+          <template slot-scope="scope">
+            <span v-if="scope.row.status === 0" class="red">禁用</span>
+            <span v-else>启用</span>
+          </template>
+        </el-table-column>
         <el-table-column label="操作">
           <!-- 插槽 -->
           <template>
-            <el-button type="primary">编辑</el-button>
+            <el-button type="text">编辑</el-button>
+            <el-button type="text">禁用</el-button>
+            <el-button type="text">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
       <!-- 分页器 -->
       <el-pagination
         background
-        :current-page="1"
-        :page-sizes="[5, 10, 15, 20]"
-        :page-size="100"
+        :current-page="page"
+        :page-sizes="pageSizes"
+        :page-size="limit"
         layout="total, sizes, prev, pager, next, jumper"
-        :total="36"
+        :total="total"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
       >
       </el-pagination>
     </el-card>
@@ -58,13 +69,68 @@
 </template>
 
 <script>
+// 导入 接口
+import { subjectApi } from "../../../api/api.js";
 export default {
   name: "subject",
   data() {
     return {
+      // 筛选的表格
       formInline: {},
-      tableData: []
+      // 数据
+      tableData: [],
+      // 页码
+      page: 1,
+      // 页容量
+      limit: 10,
+      // 页码数组
+      pageSizes:[5,10,15,20],
+      // 总条数
+      total:0
     };
+  },
+  created() {
+    // 调用接口
+    subjectApi
+      .list({
+        page: this.page,
+        limit: this.limit
+      })
+      .then(res => {
+        // window.console.log(res);
+        // 赋值给table
+        this.tableData = res.data.data.items;
+        // 保存 总条数
+        this.total = res.data.data.pagination.total;
+      });
+  },
+  // 方法
+  methods: {
+    search() {
+      // 调用接口 传递筛选条件
+      subjectApi
+        .list({ page: this.page, limit: this.limit, ...this.formInline })
+        .then(res => {
+          window.console.log(res);
+          // 赋值给table
+          this.tableData = res.data.data.items;
+        });
+    },
+    // 页容量改变
+    handleSizeChange(size){
+      // 保存起来
+      this.limit = size;
+      this.page = 1;
+      // 重新获取数据
+      this.search();
+    },
+    // 页容量改变
+    handleCurrentChange(current){
+      // 保存页码
+      this.page = current
+      // 重新获取数据
+      this.search()
+    }
   }
 };
 </script>
@@ -88,6 +154,10 @@ export default {
   // card的样式
   .main-card {
     margin-top: 20px;
+  }
+  // span 变红
+  .red {
+    color: red;
   }
 }
 </style>
